9e777c09fa5bfc1686e93ac425e6c9e2ff3fce81
Priyesh Kumar <Priyesh_Kumar@epam.com>
2020-01-14 17:12:44 +0530

    Authentication complete

--- Secrets/app.js
+++ Secrets/app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 app.set('view engine','ejs');
@@ -21,26 +24,91 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  socialPlatformId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  (accessToken, refreshToken, profile, cb)=>{
+    console.log(profile);
+    User.findOrCreate({ socialPlatformId: profile.id },(err, user)=>{
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ socialPlatformId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", (req, res)=>{
   res.render("home");
 });
+
+
+
+app.get("/auth/google",
+passport.authenticate('google', {scope: ["profile"]})
+);
+
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    res.redirect("/secrets");
+  });
+
+
+  app.get('/auth/facebook',
+    passport.authenticate('facebook', {scope: ["profile"]}));
+
+  app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/secrets');
+    });
+
+
 
 app.get("/login", (req, res)=>{
   res.render("login");
@@ -51,8 +119,19 @@ app.get("/register", (req, res)=>{
 });
 
 app.get("/secrets", (req, res)=>{
+    User.find({"secret": {$ne: null}}, (err, foundUsers)=>{
+      if(err)
+      console.log(err);
+      else {
+        if(foundUsers)
+        res.render("secrets", {usersWithSecrets:foundUsers});
+      }
+    })
+})
+
+app.get("/submit", (req, res)=>{
   if(req.isAuthenticated())
-    res.render("secrets");
+    res.render("submit");
     else {
       res.redirect("/login");
     }
@@ -63,6 +142,33 @@ app.get("/logout", (req, res)=>{
   req.logout();
   res.redirect("/");
 })
+
+
+app.post("/submit", (req, res)=>{
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, (err, foundUser)=>{
+    if(err)
+    console.log(err);
+    else {
+      if(foundUser)
+      {
+        foundUser.secret = submittedSecret;
+        foundUser.save((err)=>{
+          if(err)
+          console.log(err);
+          else
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+
+
+
+})
+
+
 
 app.post("/register", (req, res)=>{
     User.register({username: req.body.username}, req.body.password, (err, user)=>{
